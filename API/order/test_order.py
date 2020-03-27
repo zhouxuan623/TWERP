3399e845ba64f4c400f53c77636480a52fcbda41
zhouxuan623 <281449451@qq.com>
2020-03-27 11:24:37 +0800

    order

--- API/order/test_order.py
+++ API/order/test_order.py
@@ -80,9 +80,46 @@ class Test_order():
         response = home_page.json()
         assert  response['code']==0,response['code']
 
-    def test_order_getOnShipment(self,_headers):
+    def test_createLogisticOrders(self,_headers):
+        '同步托运方状态'
+        url=SYS_URL+'/order/createLogisticOrders'
+        "订单状态为出货中"
+        c_mysql=mysql(B_HOST,B_USER,B_PASSWORD,B_DATABASE)
+        sql=f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.order_status=4 and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 1;"
+        sale_order_id=(c_mysql.query(sql))[0][0]
+        data={"saleOrderIds":[sale_order_id]}
+        check_result(url, _headers, data)
+
+    def test_delivered_list(self,_headers):
+        '已出货订单列表'
+        url=SYS_URL+'/order/delivered/list'
+        data={
+                "pageNum": 1,
+                "pageSize": 10,
+                "searchName": "",
+                "accountId": "",
+                "deliveryMethodId": "",
+                "paymentMethodId": "",
+                "salesTimeEnd": "",
+                "salesTimeStart": "",
+                "isInBlackList": None
+            }
+        check_result(url, _headers, data)
+
+    def test_order_detail(self,_headers):
+        '订单详情查看'
+        sql=f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.order_status=0 and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 0;"
+        c_mysql=mysql(B_HOST,B_USER,B_PASSWORD,B_DATABASE)
+        if len(c_mysql.query(sql))==0:
+            pytest.skip(msg='订单数据为空')
+        sale_order_id = (c_mysql.query(sql))[0][0]
+        url=SYS_URL+f'/order/details/{sale_order_id}'
+        check_result(url, _headers)
+
+    @pytest.mark.parametrize('url',[SYS_URL + '/order/getOnShipment',SYS_URL + '/order/getOnShipment'])
+    def test_order_getOnShipment(self,_headers,url):
         "出货中列表查询"
-        url = SYS_URL + '/order/getOnShipment'
+        # url = SYS_URL + '/order/getOnShipment'
         home_page = requests.get(url, headers=_headers)
         response = home_page.json()
         assert response['code'] == 0

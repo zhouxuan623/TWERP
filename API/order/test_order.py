c4cd9650e2ec2ce7be9f3689416678959e9a2f9b
zhouxuan623 <281449451@qq.com>
2020-04-01 17:11:03 +0800

    order

--- API/order/test_order.py
+++ API/order/test_order.py
@@ -3,7 +3,10 @@
 author:zhouxuan
 @project--file : TWERP -->test_order.py
 2020/3/24 10:24
-@Desc:
+@Desc:订单平台状态0 核对中 , 1 新订单（已付款待配货） ,2 配货后缺货，3 配货完成  4 出货中(等待出货) ，
+5 已出货，8、待取消(平台标记取消，但用户未取消) 9 取消
+是否已付款，0未付款 1 已付款
+sync_status  同步状态:0-- 未同步  1 --同步失败  2 -- 同步中  3 --同步成功   9--取消同步
 """
 import requests
 import pytest
@@ -108,24 +111,101 @@ class Test_order():
 
     def test_order_detail(self,_headers):
         '订单详情查看'
-        sql=f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.order_status=0 and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 0;"
+        sql=f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.order_status=0 and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 1;"
         c_mysql=mysql(B_HOST,B_USER,B_PASSWORD,B_DATABASE)
         if len(c_mysql.query(sql))==0:
             pytest.skip(msg='订单数据为空')
+            c_mysql.close()
         sale_order_id = (c_mysql.query(sql))[0][0]
+        c_mysql.close()
         url=SYS_URL+f'/order/details/{sale_order_id}'
         check_result(url, _headers)
 
-    @pytest.mark.parametrize('url',[SYS_URL + '/order/getOnShipment',SYS_URL + '/order/getOnShipment'])
+    def test_getMenuCount(self,_headers):
+        u'获取菜单中各模块的数量/order/getMenuCount'
+        url=SYS_URL+'/order/getMenuCount'
+        check_result(url, _headers)
+
+    # @pytest.mark.parametrize('url',[SYS_URL + '/order/getOnShipment',SYS_URL + '/order/getOnShipment'])
     def test_order_getOnShipment(self,_headers,url):
-        "出货中列表查询"
-        # url = SYS_URL + '/order/getOnShipment'
-        home_page = requests.get(url, headers=_headers)
-        response = home_page.json()
-        assert response['code'] == 0
+        "出货中订单的配送方式数量查询/order/getOnShipment"
+        url = SYS_URL + '/order/getOnShipment'
+        check_result(url,_headers)
+
+    def test_markPaid(self,_headers):
+        u'标记已付款POST /order/markPaid'
+        url=SYS_URL+'/order/markPaid'
+        sql = f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.is_paid=0 and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 1;"
+        c_mysql = mysql(B_HOST, B_USER, B_PASSWORD, B_DATABASE)
+        if len(c_mysql.query(sql))==0:
+            pytest.skip(msg='没有订单可以标记为已付款')  #跳过此用例
+        sale_order_id = (c_mysql.query(sql))[0][0]
+        c_mysql.close()
+        data={"saleOrderIds":[f"{sale_order_id}"],"paymentType":2}
+        check_result(url,_headers,data)
+
+    def test_onshippingment_list(self,_headers):
+        "出货中订单列表/order/onShipment/list"
+        url=SYS_URL+'/order/onShipment/list'
+        data = {
+                "pageNum": 1,
+                "pageSize": 10,
+                "searchName": "",
+                "accountId": "",
+                "deliveryMethodId": "",
+                "paymentMethodId": "",
+                "salesTimeEnd": "",
+                "salesTimeStart": "",
+                "isInBlackList": None
+            }
+        check_result(url,_headers,data)
+
+    def test_pickingChecking(self,_headers):
+        '新订单配货 /order/pickingChecked'
+        url = SYS_URL+'/order/pickingChecked'
+        data = {"saleOrderIds":[sale_id(1)]}
+        check_result(url,_headers,data)
+
+    def test_picking_stockout(self,_headers):
+        '缺货订单配货/order/pickingStockout'
+        url = SYS_URL+"/order/pickingStockout"
+        data = {"saleOrderIds": [sale_id(2)]}
+        check_result(url, _headers, data)
+
+    def test_printLabelList(self,_headers):
+        "打印出货标签 /order/printLabelList"
+        url = SYS_URL+'/order/printLabelList'
+        data = {
+                  "saleOrderIds": [
+                      other_sale_id('a.sync_status=3')
+                  ]
+        }
+        check_result(url,_headers, data)
+
+
+
+
+def sale_id(order_status):
+    sql = f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where a.is_paid='{order_status}' and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 1;"
+    c_mysql = mysql(B_HOST, B_USER, B_PASSWORD, B_DATABASE)
+    if len(c_mysql.query(sql)) == 0:
+        pytest.skip(msg='没有匹配的订单')  # 跳过此用例
+    sale_order_id = (c_mysql.query(sql))[0][0]
+    return sale_order_id
+
+def other_sale_id(sql_condition):
+    sql = f"SELECT a.sale_order_id from {B_DATABASE}.tb1_sale_orders a where {sql_condition} and merchant_id='{MERCHANT}' ORDER BY a.sales_time desc limit 1;"
+    c_mysql = mysql(B_HOST, B_USER, B_PASSWORD, B_DATABASE)
+    if len(c_mysql.query(sql)) == 0:
+        pytest.skip(msg='没有匹配的订单')  # 跳过此用例
+    sale_order_id = (c_mysql.query(sql))[0][0]
+    return sale_order_id
+
+
+
 
 
 
 
 if __name__ == '__main__':
-    pytest.main(['-s','test_order.py'])
+    pytest.main(['-v','test_order.py::Test_order::test_printLabelList'])

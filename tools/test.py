a23abadb37de495b9f7f6569e7e9317e876a5c63
zhouxuan623 <281449451@qq.com>
2020-05-14 14:02:59 +0800

    init

--- tools/test.py
+++ tools/test.py
@@ -5,13 +5,18 @@ author:zhouxuan
 2020/2/28 17:11
 @Desc:
 """
-def aa():
-    return __name__
-
-
-aa=12
-value=f'fd{aa}'
+def updateDict(paramdic,value):
+    sm={}
+    for key,value1 in paramdic.items():
+        if key == "key":
+            sm[key] = value
+        else:
+            sm[key] = value1
+    return sm
 
 if __name__ == '__main__':
     # print (aa())
-    print (value)
+    data={"key":'0000',"key1":"1111"}
+    m=updateDict(data,"mmmm")
+    n=updateDict(data,'nnnn')
+    print(m,n)
